58fc825d386a149889594c53bbf75b29da56cfd0
ihatelethargy <naturalflavor1007@naver.com>
2020-11-09 15:48:18 +0900

    pageInfo 수정 + pageCounter 오류 수정

--- tumblbug_3주차/main.js
+++ tumblbug_3주차/main.js
@@ -2,15 +2,66 @@ const slideContainer = document.querySelector('.container');
 const slide = document.querySelector('.slides');
 const nextBtn = document.getElementById('next-btn');
 const prevBtn = document.getElementById('prev-btn');
-const interval = 1000;
+const interval = 4000;
 const page = document.getElementById('page');
+const title = document.getElementById('title-item');
+const desc = document.getElementById('desc-item');
+const backColor = document.getElementById('card-background');
 
+
+let pageInfo = [
+    {
+     pageNum: 1,
+     pageColor: '#222222',
+     pageTitle: '2030년 마침내<br> 인류최후의 유토피아',
+     pageDesc: '전원이 힘을 모아서 기술 중심 유토피아 건설하기',
+    },
+    {
+     pageNum: 2,
+     pageColor: '#358EB5',
+     pageTitle: '동그란 반지는<br>고정관념이지',
+     pageDesc: '창에 비치는 달빛을 모티브로 한 사각 반지',
+    },
+    {
+     pageNum: 3,
+     pageColor: '#D77847',
+     pageTitle: '성별에 상관없이<br>멋진 우리는',
+     pageDesc: '말랑과 샤이엔, 트렌스젠더입니다.',
+    },
+    {
+     pageNum: 4,
+     pageColor: '#F3ADAC',
+     pageTitle: '발그레한 장미색은<br>어떻게 만들어질까',
+     pageDesc: '디자이너가 직접만든 CMYK 컬러배합 맞추기',
+    },
+    {
+     pageNum: 5,
+     pageColor: '#2EA68A',
+     pageTitle: '500년의 폐기물보다<br>500년의 지구를 위해',
+     pageDesc: 'CLAP 3기에서 플라스틱 Free 창작자를 찾습니다',
+    }
+]
+
+let color = ['#222222','#358EB5','#D77847','#F3ADAC','#2EA68A'];
 let slides = document.querySelectorAll('.slide');
 let index = 1;
 let slideId; 
+let realCounter = 1;
+
 
 const pageAdder = () => {
-    page.innerHTML = `${index}/5`
+    realCounter = index;
+    if(realCounter === 6){
+        realCounter = 1;
+    }
+    else if (realCounter === 0){
+        realCounter = 5;
+    }
+    console.log(realCounter);
+    page.innerHTML = `${realCounter}/5`;
+    title.innerHTML = `${pageInfo[realCounter-1].pageTitle}`;
+    desc.innerHTML = `${pageInfo[realCounter-1].pageDesc}`;
+    backColor.style.background = `${pageInfo[realCounter-1].pageColor}`;
 }
 
 const firstClone = slides[0].cloneNode(true);
@@ -54,18 +105,23 @@ slide.addEventListener('transitionend', ()=>{
 });
 
 const moveToNextSlide = () =>{
-        pageAdder();
+        
             slides = document.querySelectorAll('.slide');
             if(index >= slides.length - 1) return;
         index++;
+        pageAdder(); // Here
+        
         slide.style.transform = `translateX(${-slideWidth * index}px)`; 
         slide.style.transition = '.8s';
 }
 
 const moveToPrevSlide = () =>{
-        pageAdder();
+        
+        
             if(index <= 0) return;
         index--;
+        pageAdder(); // Here
+        
         slide.style.transform = `translateX(${-slideWidth * index}px)`; 
         slide.style.transition = '.8s';
 }
@@ -82,4 +138,3 @@ nextBtn.addEventListener('click', moveToNextSlide);
 prevBtn.addEventListener('click', moveToPrevSlide);
 
 startSlide();
-
